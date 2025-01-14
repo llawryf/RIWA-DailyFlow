7580875d83f87c89161816b5745cce01dd9af07c
lmajetic <lukamajeticc@gmail.com>
2025-01-14 09:29:33 +0100

    Admin page

--- frontend/quasar-project/src/pages/AdminPage.vue
+++ frontend/quasar-project/src/pages/AdminPage.vue
@@ -1,11 +1,17 @@
 <template>
-  <q-page padding>
-    <!-- content -->
+  <q-page class="flex flex-center">
+    <div class="q-pa-md q-gutter-sm">
+
+      <q-btn color="purple" label="Upravljanje receptima" to="/admin/recepti" />
+      <q-btn color="purple" label="Upravljanje korisnicima" to="/admin/korisnici" />
+      <q-btn color="purple" label="Logout" to="/admin/logout" />
+
+  </div>
   </q-page>
 </template>
 
-<script>
-export default {
-  // name: 'PageName',
-}
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
 </script>
