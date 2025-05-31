c5fb42792e01fa32e5ba6c6f7a0c21faf71b1144
llawryf <lcelcner@veleri.hr>
2025-05-31 10:58:29 +0200

    kad se user ulogira neka se upiše Welcome, njegov username

--- frontend/quasar-project/src/layouts/MainLayout.vue
+++ frontend/quasar-project/src/layouts/MainLayout.vue
@@ -1,8 +1,7 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar class="text-white relative" style="background-color: #272727;">
-
+      <q-toolbar class="text-white relative" style="background-color: #272727">
         <q-btn
           flat
           dense
@@ -14,40 +13,43 @@
 
         <q-toolbar-title class="q-pa-none">
           <div class="absolute-center">
-            <img src="https://imgur.com/XCLanXO.png" style="height: 40px; padding-top:15px; height:60px">
+            <img
+              src="https://imgur.com/XCLanXO.png"
+              style="height: 40px; padding-top: 15px; height: 60px"
+            />
           </div>
         </q-toolbar-title>
-
       </q-toolbar>
     </q-header>
 
     <q-drawer
-  v-model="leftDrawerOpen"
-  show-if-above
-  bordered
-  class="text-white" style="background-color: #272727;"
->
-  <q-list class="text-white" style="background-color:#272727">
-    <q-item-label header class="text-white">
-      Essential Links
-    </q-item-label>
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      class="text-white"
+      style="background-color: #272727"
+    >
+      <q-list class="text-white" style="background-color: #272727">
+        <q-item-label header class="text-white"> Essential Links </q-item-label>
 
-    <q-item v-if="isLoggedIn">
-      <q-item-section>
-        <q-item-label class="text-white">Welcome, {{ username }}</q-item-label>
-      </q-item-section>
-    </q-item>
+        <q-item v-if="isLoggedIn">
+          <q-item-section>
+            <q-item-label class="text-white"
+              >Welcome, {{ username }}</q-item-label
+            >
+          </q-item-section>
+        </q-item>
 
-    <EssentialLink
-      v-for="link in linksList"
-      :key="link.title"
-      :title="link.title"
-      :caption="link.caption"
-      :icon="link.icon"
-      :to="link.link"
-    />
-  </q-list>
-</q-drawer>
+        <EssentialLink
+          v-for="link in linksList"
+          :key="link.title"
+          :title="link.title"
+          :caption="link.caption"
+          :icon="link.icon"
+          :to="link.link"
+        />
+      </q-list>
+    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -55,90 +57,105 @@
   </q-layout>
 </template>
 
-
-
-
 <script setup>
-import { ref, watch, onMounted, watchEffect } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { ref, watch, onMounted, watchEffect } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+import { useRoute } from "vue-router";
 
 defineOptions({
-  name: 'MainLayout'
-})
+  name: "MainLayout",
+});
 
 const linksList = [
   {
-    title: 'Naslovna',
-    caption: 'početna stranica',
-    icon: 'home',
-    link: '/'
+    title: "Naslovna",
+    caption: "početna stranica",
+    icon: "home",
+    link: "/",
   },
   {
-    title: 'Pretraživanje',
-    caption: 'pretraživanje recepata',
-    icon: 'search',
-    link: '/pretraga_recepata'
+    title: "Pretraživanje",
+    caption: "pretraživanje recepata",
+    icon: "search",
+    link: "/pretraga_recepata",
   },
   {
-    title: 'Login',
-    caption: 'Prijava',
-    icon: 'login',
-    link: '/login'
+    title: "Login",
+    caption: "Prijava",
+    icon: "login",
+    link: "/login",
   },
   {
-    title: 'Register',
-    caption: 'Registracija',
-    icon: 'how_to_reg',
-    link: '/register'
+    title: "Register",
+    caption: "Registracija",
+    icon: "how_to_reg",
+    link: "/register",
   },
   {
-    title: 'Profil',
-    caption: 'Otvori moj profil',
-    icon: 'account_circle',
-    link: '/profil'
+    title: "Profil",
+    caption: "Otvori moj profil",
+    icon: "account_circle",
+    link: "/profil",
   },
   {
-    title: 'Spremljeni recepti',
-    caption: 'Pregled spremljenih recepata',
-    icon: 'menu_book',
-    link: '/spremljeni_recepti'
+    title: "Spremljeni recepti",
+    caption: "Pregled spremljenih recepata",
+    icon: "menu_book",
+    link: "/spremljeni_recepti",
   },
   {
-    title: 'Izrada recepta',
-    caption: 'Izradi svoj recept',
-    icon: 'edit_note',
-    link: '/izrada_recepta'
+    title: "Izrada recepta",
+    caption: "Izradi svoj recept",
+    icon: "edit_note",
+    link: "/izrada_recepta",
   },
   {
-    title: 'Pretraga korisnika',
-    caption: 'Pretraži korisnike',
-    icon: 'person_search',
-    link: '/pretraga_korisnika'
-  }
-]
+    title: "Pretraga korisnika",
+    caption: "Pretraži korisnike",
+    icon: "person_search",
+    link: "/pretraga_korisnika",
+  },
+];
 
-const leftDrawerOpen = ref(false)
+const leftDrawerOpen = ref(false);
 
 // Check if user is logged in
-const isLoggedIn = ref(false)
-const username = ref('')
+const isLoggedIn = ref(false);
+const username = ref("");
 
 // Fetch the username and token from sessionStorage
 watchEffect(() => {
-  const token = sessionStorage.getItem('authToken')
-  const storedUsername = sessionStorage.getItem('username')
+  const token = sessionStorage.getItem("authToken");
+  const storedUsername = sessionStorage.getItem("username");
 
   if (token && storedUsername) {
-    isLoggedIn.value = true
-    username.value = storedUsername
+    isLoggedIn.value = true;
+    username.value = storedUsername;
   } else {
-    isLoggedIn.value = false
-    username.value = ''
+    isLoggedIn.value = false;
+    username.value = "";
   }
-})
+});
 
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
 }
-</script>
 
+const route = useRoute();
+
+watch(
+  () => route.fullPath,
+  () => {
+    const token = sessionStorage.getItem("authToken");
+    const storedUsername = sessionStorage.getItem("username");
+
+    if (token && storedUsername) {
+      isLoggedIn.value = true;
+      username.value = storedUsername;
+    } else {
+      isLoggedIn.value = false;
+      username.value = "";
+    }
+  }
+);
+</script>
--- frontend/quasar-project/src/pages/LoginPage.vue
+++ frontend/quasar-project/src/pages/LoginPage.vue
@@ -32,97 +32,104 @@
       </div>
 
       <!-- Submit button -->
-      <q-btn label="Potvrdi" color="primary" type="submit" :loading="isLoading" />
+      <q-btn
+        label="Potvrdi"
+        color="primary"
+        type="submit"
+        :loading="isLoading"
+      />
     </q-form>
 
     <!-- Loading spinner indicator -->
-    <div v-if="isLoading" class="q-mt-md" style="text-align: center;">
+    <div v-if="isLoading" class="q-mt-md" style="text-align: center">
       <q-spinner-dots size="50px" color="primary" />
     </div>
   </q-page>
 </template>
 
 <script>
-import axios from 'axios';
-import { useQuasar } from 'quasar';
+import axios from "axios";
+import { useQuasar } from "quasar";
 
 export default {
-  name: 'LoginPage',
+  name: "LoginPage",
 
   data() {
     return {
       // Object to store user input
       loginData: {
-        username: '',
-        password: '',
+        username: "",
+        password: "",
       },
 
       // State variable to track loading state
-      isLoading: false
+      isLoading: false,
     };
   },
 
   mounted() {
     // Provjera ako je vec netko ulogiran
-    const token = sessionStorage.getItem('authToken');
+    const token = sessionStorage.getItem("authToken");
 
     if (token) {
       // Ako da, ridirekcija na index page
-      this.$router.push('/');
+      this.$router.push("/");
     }
   },
 
   methods: {
-  async submitLogin() {
-    if (!this.loginData.username || !this.loginData.password) {
-      this.$q.notify({
-        type: 'negative',
-        message: 'Molimo vas da unesete korisničko ime i lozinku.',
-      });
-      return;
-    }
-
-    this.isLoading = true;
-
-    const loginData = {
-      username: this.loginData.username,
-      password: this.loginData.password,
-    };
-
-    try {
-      const response = await axios.post("http://localhost:3000/login", loginData);
-
-      if (response.data.success) {
-        sessionStorage.setItem('authToken', response.data.token);
-        sessionStorage.setItem('username', this.loginData.username);
-
+    async submitLogin() {
+      if (!this.loginData.username || !this.loginData.password) {
         this.$q.notify({
-          type: 'positive',
-          message: 'Prijava uspješna!',
+          type: "negative",
+          message: "Molimo vas da unesete korisničko ime i lozinku.",
         });
-
-        // Dynamically update the username in MainLayout
-        if (this.loginData.username === 'Admin 1') {
-          this.$router.push('/admin');
-        } else {
-          this.$router.push('/');
-        }
-      } else {
-        this.$q.notify({
-          type: 'negative',
-          message: response.data.message,
-        });
+        return;
       }
-    } catch (error) {
-      this.$q.notify({
-        type: 'negative',
-        message: 'Došlo je do greške. Pokušajte ponovo.',
-      });
-    } finally {
-      this.isLoading = false;
-    }
-  }
-}
 
+      this.isLoading = true;
+
+      const loginData = {
+        username: this.loginData.username,
+        password: this.loginData.password,
+      };
+
+      try {
+        const response = await axios.post(
+          "http://localhost:3000/login",
+          loginData
+        );
+
+        if (response.data.success) {
+          sessionStorage.setItem("authToken", response.data.token);
+          sessionStorage.setItem("username", this.loginData.username);
+
+          this.$q.notify({
+            type: "positive",
+            message: "Prijava uspješna!",
+          });
+
+          // Dynamically update the username in MainLayout
+          if (this.loginData.username === "Admin 1") {
+            this.$router.push("/admin");
+          } else {
+            this.$router.push("/");
+          }
+        } else {
+          this.$q.notify({
+            type: "negative",
+            message: response.data.message,
+          });
+        }
+      } catch (error) {
+        this.$q.notify({
+          type: "negative",
+          message: "Došlo je do greške. Pokušajte ponovo.",
+        });
+      } finally {
+        this.isLoading = false;
+      }
+    },
+  },
 };
 </script>
